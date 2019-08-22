8ab084c11e34a268b0f5352bc9350f9f349908c1
jc428214 <brody.jorgensen@my.jcu.edu.au>
2019-08-22 11:41:00 +1000

    correct the formatting of the pracs

--- prac_2/Capitalist_Conrad.py
+++ prac_2/Capitalist_Conrad.py
@@ -13,15 +13,10 @@ print("The starting price is: ${:,.2f}".format(price), file=out_file)
 while price >= MIN_PRICE and price <= MAX_PRICE:
     price_change = 0
     day += 1
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
+
     if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
         price_change = random.uniform(0, MAX_INCREASE)
     else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
     price *= (1 + price_change)
     print(" On day {} the price is: ${:,.2f}".format(day, price), file=out_file)
