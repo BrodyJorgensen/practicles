6eb68e3073868f43b247fda44e92c20443da9cf7
jc428214 <brody.jorgensen@my.jcu.edu.au>
2019-09-26 10:21:41 +1000

    Start the 7th prac

---
+++ prac_7/convert_m_km.py
@@ -0,0 +1,37 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
+
+
+class MilesConverterApp(App):
+    output_km = StringProperty()
+
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_m_km.kv')
+        return self.root
+
+    def handle_calculate(self, number):
+        miles = self.convert_to_number(self.number)
+        self.update_result(miles)
+        return number
+
+    def handle_increment(self, number, change):
+        miles = self.convert_to_number(self.number) + change
+        self.root.ids.input_miles.text = str(miles)
+        # Since the InputText.text has changed, its on_text event will fire and handle_calculate will be called
+
+    def update_result(self, miles):
+        self.output_km = str(miles * MILES_TO_KM)
+
+    def convert_to_number(self.number):
+        try:
+            value = float(number)
+            return value
+        except ValueError:
+            return 0
+
+
+MilesConverterApp().run()
